add4a9691f87a08b185a96f81f351a03f57c5c9c
Abbas463 <abbashilal463@gmail.com>
2026-03-05 12:06:14 +0430

    Working

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from data-dashboard!")
+import tkinter as tk
+from tkinter import ttk
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-if __name__ == "__main__":
-    main()
+class SimpleDashboard:
+    def __init__ (self, root):
+        self.root = root
+
+        self.root.title("Simple Data Dashboard")
+        self.root.geometry("1000x600")
+
+        df = pd.read_csv('titanic.csv')
+
+        control_frame = tk.Frame(root)
+        control_frame.pack(pady=10)
+
+        tk.Label(control_frame, text="Column:").pack(side=tk.LEFT)
+        self.column_var = tk.StringVar(value='Age')
+
+        combo = ttk.Combobox(control_frame, textvariable=self.column_var, values=['Age', 'Fare'], width=10)
+        combo.pack(side=tk.LEFT, padx=10)
+        combo.bind("<<ComboboxSelected>>", self.update_chart)
+    
+    def update_chart(self, event=None):
+        pass
